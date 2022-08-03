7d573ff90f1f46d3195e40baede3e33986f4d266
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-02 20:22:41 -0700

    redid html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // import functions and grab DOM elements
 
-const currentPollEL = document.getElementById('current-poll-container');
+const currentPollEl = document.getElementById('current-poll-container');
 const newPollEl = document.getElementById('new-poll');
 
 
@@ -10,22 +10,24 @@ const newPollEl = document.getElementById('new-poll');
 let question = '';
 let optionA = '';
 let optionB = '';
-let votesA = 0;
-let votesB = 0;
+let votesOne = 0;
+let votesTwo = 0;
 let pastPolls = [];
 // set event listeners 
 newPollEl.addEventListener('submit', (e) => {
     e.preventDefault();
-    const data = new FormData(newPollEl);
+    const data = new FormData(currentPollEl);
     const userQuestion = data.get('question');
     const userOptionA = data.get('option-A');
     const userOptionB = data.get('option-B');
-
+    const votesOne = data.get('vote-one');
+    const votesTwo = data.get('vote-two');
+    
     question = userQuestion;
     optionA = userOptionA;
     optionB = userOptionB;
-    votesA = userVotesA;
-    VotesB = userVotesB;
+    votesOne = userVotesA;
+    votesTwo = userVotesB;
 
     renderPoll();
     displayCurrentPoll();
@@ -38,4 +40,23 @@ newPollEl.addEventListener('submit', (e) => {
   // update DOM to reflect the new state
 
 
-  function renderPoll()
+function renderPoll(question, optionA, optionB, votesOne, votesTwo) {
+    const currentPollEl = document.createElement('div');
+    const questionEl = document.createElement('p');
+    questionEl.textContent = question;
+    const optionAEl = document.createElement('p');
+    optionAEl.textContent = optionA;
+    const optionBEl = document.createElement('p');
+    optionBEl.textContent = optionB;
+    const votesOneEl = document.createElement('p');
+    votesOneEl.textContent = votesOne;
+    const votesTwoEl = document.createElement('p');
+    votesTwoEl.textContent = votesTwo;
+
+    currentPollEl.append(question, optionA, optionB, votesOne, votesTwo);
+
+    return currentPollEl;
+
+  
+  
+}
