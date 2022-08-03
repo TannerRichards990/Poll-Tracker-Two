287df7c28635ec7426b4ffa87370de1eeb083a79
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-02 17:20:48 -0700

    added DOM elements, state, and newpollEvenList. Working on renderPoll atm

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 // import functions and grab DOM elements
 
-// let state
+const currentPollEL = document.getElementById('current-poll-container');
+const newPollEl = document.getElementById('new-poll');
 
+
+
+
+// let state
+let question = '';
+let optionA = '';
+let optionB = '';
+let votesA = 0;
+let votesB = 0;
+let pastPolls = [];
 // set event listeners 
+newPollEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(newPollEl);
+    const userQuestion = data.get('question');
+    const userOptionA = data.get('option-A');
+    const userOptionB = data.get('option-B');
+
+    question = userQuestion;
+    optionA = userOptionA;
+    optionB = userOptionB;
+    votesA = userVotesA;
+    VotesB = userVotesB;
+
+    renderPoll();
+    displayCurrentPoll();
+
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+  function renderPoll()
